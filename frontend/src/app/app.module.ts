bab3f918d3c53d730b3e2810e75a58d5f76af3a4
rigole <foplacide@gmail.com>
2023-12-20 17:29:17 +0100

    adding and styling the header + creating the input component

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TaskListComponent } from './task-list/task-list.component';
 import { HeaderComponent } from './header/header.component';
+import { InputComponent } from './input/input.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     TaskListComponent,
-    HeaderComponent
+    HeaderComponent,
+    InputComponent
+   
   ],
   imports: [
     BrowserModule,
