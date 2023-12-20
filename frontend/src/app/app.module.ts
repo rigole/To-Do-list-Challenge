d330a9ce75b9e883f3a50126abc11cf8a3d1e915
rigole <foplacide@gmail.com>
2023-12-20 11:02:19 +0100

    adding a search bar for task

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TaskListComponent } from './task-list/task-list.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TaskListComponent
+    TaskListComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
